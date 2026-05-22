9897ab844a4e70942760ca2823288120acf2be50
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-22 19:04:25 +0530

    Initialse gooddata.jsx

---
+++ components/goodData.jsx
@@ -0,0 +1,10 @@
+
+const GoodData = ()=>{
+    return(
+        <>
+        <div className="text-center text-2xl font-semibold text-neutral-900 text-shadow-md bg-red-200 mb-12">
+            <h1>From here the Better adn optimised code quality is present</h1>
+        </div>
+        </>
+    )
+}
