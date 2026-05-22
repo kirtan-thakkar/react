1de5def628c6b394a59208a570a99ab919eaff66
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-22 18:41:01 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-semibold text-neutral-900 text-shadow-md">
+        Hello this application is made for testing the code quality of mine and i have to make sure whenever i am using the useState and useEffect i have to go and make its custom hook to make the code maintaneble for future use!
+      </h1>
+      
+    </div>
+  );
+}
