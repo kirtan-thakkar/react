be2e8e5e7067753abed4d40a3a27dd2cba8a2303
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-22 19:08:18 +0530

    Add useProduct hook

--- components/goodData.jsx
+++ components/goodData.jsx
@@ -1,10 +1,13 @@
-
+import Image from "next/image";
+import useProduct from "./hooks/useProduct";
 const GoodData = ()=>{
     return(
         <>
         <div className="text-center text-2xl font-semibold text-neutral-900 text-shadow-md bg-red-200 mb-12">
             <h1>From here the Better adn optimised code quality is present</h1>
         </div>
+
+        
         </>
     )
 }
---
+++ components/hooks/useProduct.jsx
@@ -0,0 +1,19 @@
+import {useState, useEffect} from "react";
+
+const useProduct = ()=>{
+    const [products,setProducts] = useState([]);
+
+    const getData = async()=>{
+    const data = await fetch("https://dummyjson.com/products");
+    const jsonData = await data.json();
+    setProducts(jsonData.products ?? []);
+    }
+
+    useEffect(()=>{
+        getData();
+    },[])
+
+    return {products}
+
+};
+export default useProduct;
