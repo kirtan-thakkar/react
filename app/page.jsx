fa556a091ee50d8f0281267088382e1af619fc11
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-22 19:59:39 +0530

    Add better and maintainable version by clubbing useeffect and useState

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import FakeData from "@/components/fakeData";
+import GoodData from "@/components/goodData";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <h1 className="text-center text-4xl font-semibold text-neutral-900 text-shadow-md mb-12">
         Hello this application is made for testing the code quality of mine and i have to make sure whenever i am using the useState and useEffect i have to go and make its custom hook to make the code maintaneble for future use!
       </h1>
-      <FakeData />
+      <GoodData />
     </div>
   );
 }
--- components/goodData.jsx
+++ components/goodData.jsx
@@ -1,13 +1,39 @@
+"use client";
+
 import Image from "next/image";
 import useProduct from "./hooks/useProduct";
-const GoodData = ()=>{
-    return(
-        <>
-        <div className="text-center text-2xl font-semibold text-neutral-900 text-shadow-md bg-red-200 mb-12">
-            <h1>From here the Better adn optimised code quality is present</h1>
-        </div>
+const GoodData = () => {
+    const {products} = useProduct();
+  return (
+    <>
+      <div className="text-center text-2xl font-semibold text-neutral-900 text-shadow-md bg-red-200 mb-12">
+        <h1>From here the Better adn optimised code quality is present</h1>
+      </div>
 
-        
-        </>
-    )
-}
+
+      <div className="flex flex-wrap h-48 w-full justify-center items-start gap-6 ">
+        {products.map((item) => (
+          <div
+            className="border border-neutral-700 shadow-md rounded-lg mx-auto max-w-4xl w-full px-4 py-6"
+            key={item.id}
+          >
+            <div key={item.id}>
+              <h1 className="text-xl font-bold text-neutral-900">
+                {item.title}
+              </h1>
+              <p className="text-gray-600">{item.description}</p>
+              <Image
+                src={item.thumbnail}
+                alt={item.title}
+                width={200}
+                height={50}
+                className="object-cover rounded-md bg-neutral-100 h-50 mx-auto w-full max-w-md"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+export default GoodData;
