366afe74b54f91c8a7e37afd6bde9bb4dfdfff20
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-23 11:56:45 +0530

    Add useForm hook

--- components/form.jsx
+++ components/form.jsx
@@ -1,7 +1,22 @@
 const formPage = ()=>{
+    
     return(
         <div>
-            <h1>Form </h1>
+            <form>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required />
+                <br />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+                <br />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+                <br />
+                <label htmlFor="confirmPassword">Confirm Password:</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" required />
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
---
+++ components/hooks/useForm.jsx
@@ -0,0 +1,21 @@
+"use client";
+import {useState} from "react";
+import formValidationSchema from "@/validation/zod";
+const [formData,setFormData] = useState({
+    name:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+})
+const handleSubmit = (e)=>{
+    e.preventDefault();
+    try{
+        formValidationSchema.parse(formData);
+        // If validation passes, you can proceed with form submission logic here
+        console.log("Form data is valid:", formData);
+    } catch (error) {
+        console.error("Form validation error:", error);
+    }
+}
+
+export {handleSubmit,formData}
