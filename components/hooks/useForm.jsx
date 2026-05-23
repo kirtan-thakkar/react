acf4b8f6b3d8602ce8f932ba14608b5092dcb5d4
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-23 11:59:02 +0530

    Update useForm

--- components/hooks/useForm.jsx
+++ components/hooks/useForm.jsx
@@ -1,21 +1,23 @@
 "use client";
-import {useState} from "react";
+import { useState } from "react";
 import formValidationSchema from "@/validation/zod";
-const [formData,setFormData] = useState({
-    name:"",
-    email:"",
-    password:"",
-    confirmPassword:""
-})
-const handleSubmit = (e)=>{
+const useForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleSubmit = (e) => {
     e.preventDefault();
-    try{
-        formValidationSchema.parse(formData);
-        // If validation passes, you can proceed with form submission logic here
-        console.log("Form data is valid:", formData);
+    try {
+      formValidationSchema.parse(formData);
+      // If validation passes, you can proceed with form submission logic here
+      console.log("Form data is valid:", formData);
     } catch (error) {
-        console.error("Form validation error:", error);
+      console.error("Form validation error:", error);
     }
-}
+  };
+};
 
-export {handleSubmit,formData}
+export { handleSubmit, formData };
