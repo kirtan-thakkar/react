07caf78e9f6fdff8fa5d26ab70b5ebbf5f4874eb
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-08-31 22:49:16 +0530

    Uncomment FormPage component

--- app/page.jsx
+++ app/page.jsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div>
       {/* <h1 className="text-center text-4xl font-semibold text-neutral-900 text-shadow-md mb-12">
-        Hello this application is made for testing the code quality of mine and i have to make sure whenever i am using the useState and useEffect i have to go and make its custom hook to make the code maintaneble for future use!
+        Hello this application is made for testing the code quality of mine and i have to make sure whenever i am using the useState and useEffect i have to go and make its custom hook to make the cod[...]
       </h1>
       <GoodData /> */}
-      {/* <FormPage /> */}
+      <FormPage />
       <WebSocketPage />
     </div>
   );
